6e57d2acdd6ad53c514861f92917dd9a353722d6
chahatjaink <chahatjaink@gmail.com>
2023-11-06 00:00:43 +0530

    refactor: code optimize

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,7 @@
-import { useContext } from 'react';
 import { DUMMY_PRODUCTS } from '../dummy-products.js';
 import Product from './Product.jsx';
-import { CartContext } from '../store/shopping-cart-context.jsx';
 
 export default function Shop() {
-  const {items,addItemToCart} =useContext(CartContext)
   return (
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
@@ -12,7 +9,7 @@ export default function Shop() {
       <ul id="products">
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={addItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </ul>
